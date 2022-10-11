99fb7f579a73859ac7e7f0f5bdf7c514079d67c1
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2022-10-11 10:34:27 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -2,9 +2,9 @@ var who65='world';
 function sayhello65(){
  alert('Hello,' + who65 + '!');
 }
-function saytaro65(){
+function taro65(){
  who65='太郎';
 }
-function sayhanako65(){
+function hanako65(){
  who65='花子';
 }
