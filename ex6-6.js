450a48c52a225bdfad96c32663b1410b0d46f056
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2022-10-11 19:15:32 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,6 +1,6 @@
 var who66='world';
 function saycome66(){
-  alert(who66 +'が来た');
+  alert(who66 + 'が来た');
 }
 function taro66(){
  who66='太郎'
@@ -8,3 +8,4 @@ function taro66(){
 function hanako66(){
  who66='花子'
 }
+A
