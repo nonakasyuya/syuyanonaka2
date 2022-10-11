62f0ace568d4136fae0bf793935a5893c1af98e8
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2022-10-12 00:42:14 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -2,8 +2,8 @@ var who65='world';
 function sayhello65(){
  alert('Hello,' + who65 + '!');
 }
-function tomoharu65(){
- who65='友治';
+function taro65(){
+ who65='太郎';
 }
 function hanako65(){
  who65='花子';
