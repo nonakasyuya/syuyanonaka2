43e15ebf7c3db5e239d03a93510f2241db1f1b46
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2022-11-08 10:03:29 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,3 +1,3 @@
 function imgchange688(){
-document.getElementById('who688').innerHTML='<./image/城西大学マスコット.png alt="城西マスコット">' ;
-}
+document.getElementById('who688').innerHTML='<./image/城西大学マスコット.png alt="城西マスコット">' 
+;}
