f62b14117aacddb3ab958d502b3de01426e345f2
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2022-11-29 10:15:32 +0900

    Delete ex6-12.js

--- ex6-12.js
+++
@@ -1,8 +0,0 @@
-function hyoji(){
-var str="一文字ずつ表示します。";
-var cnt=document.timer.moji.value.length;
-if(cnt<11){
-document.timer.moji.value=str.substr(0,cnt+1);}
-else{
-document.timer.moji.value="";}
-setTimeout("hyoji()",1000);}
