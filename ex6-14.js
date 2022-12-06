c73ee84a39384d56128dc926c48d9f936846b2eb
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2022-12-06 10:35:49 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,4 +1,16 @@
-function hyoji14(){
-var image14='./image/城西大学野球部.jpg'
-setTimeout(0,3000)
+function imgchange1(){
+ document.getElementById('logo').src=
+'./image/城西大学ロゴ.png';
 }
+function imgchange2(){
+ document.getElementById('logo').src=
+'./image/城西大学野球部.jpg';
+}
+
+
+
+
+//function hyoji14(){
+//var image14='./image/城西大学野球部.jpg'
+//setTimeout(0,3000)
+//}
