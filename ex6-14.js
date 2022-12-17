1dd867436a5caa5bf9864960bb2a5fe4182e80bb
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2022-12-17 10:17:58 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,33 +1,10 @@
-//function imgchange3(){
-//document.getElementById('logo2').src=
-//'./image/城西大学ロゴ.png';
-//}
 function imgchange4(){
- var cfn14=document.timer14.hyoji14.src;
- if(cfn14=="./image/城西大学ロゴ.png"){
- document.getElementById('logo2').src="./image/城西大学野球部.jpg";}
- else{
- setTimeout("imgchange5()",3000);}
- }
- 
+ document.getElementById('logo2').src=
+'./image/josairogo.png';
+}
+
 function imgchange5(){
-document.getElementById('logo2').src="./image/城西大学ロゴ.png";
+ document.getElementById('logo2').src=
+'./image/josaibase.jpg';
+setTimeout("imgchange4()",3000);
 }
-
-function startfnc14(){
-setInterval("imgchange4",1000)
-}
-
-//function imagchange5(){
-  //document.getElementById('logo2').src=
-//'./image/城西大学ロゴ.png';
-// setTimeout("imgchange6,200")
-//}
-
-
-
-
-//function hyoji14(){
-//var image14='./image/城西大学野球部.jpg'
-//setTimeout(0,3000)
-//}
