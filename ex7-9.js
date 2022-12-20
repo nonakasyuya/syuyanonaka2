6a2079070e58f7641e0b9c3b5a510f3768c78c75
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2022-12-20 10:34:49 +0900

    Create ex7-9.js

---
+++ ex7-9.js
@@ -0,0 +1,14 @@
+function nazo79(){
+ if(document.getElementById('answer79').value=='夢'){
+  alert('あたり！');
+ }
+  else if(document.getElementById('answer79').value=='ゆめ'){
+   alert('あたり！')
+  }
+  else if(document.getElementById('answer79').value=='ユメ'){
+   alert('あたり！')
+  }
+   else if(document.getElementById('answer79').value=='yume'){
+   alert('あたり！')
+  }
+}
