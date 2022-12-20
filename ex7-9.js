db599f388c21c1f28f952a3dfe9aadf21a1cff10
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2022-12-20 10:39:17 +0900

    Update ex7-9.js

--- ex7-9.js
+++ ex7-9.js
@@ -3,12 +3,15 @@ function nazo79(){
   alert('あたり！');
  }
   else if(document.getElementById('answer79').value=='ゆめ'){
-   alert('あたり！')
+   alert('あたり！');
   }
   else if(document.getElementById('answer79').value=='ユメ'){
-   alert('あたり！')
+   alert('あたり！');
   }
    else if(document.getElementById('answer79').value=='yume'){
-   alert('あたり！')
+   alert('あたり！');
+  }
+  else{
+   alert('はずれ…');
   }
 }
