c8e6a36ee9f8e9eaebe4a0925b5db0fe87e9c2f5
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2022-12-20 10:45:02 +0900

    Create ex7-10.js

---
+++ ex7-10.js
@@ -0,0 +1,11 @@
+function bmi710(){
+ if(document.getElementById('answer710').value<=18.5){
+ alert('低体重');
+ }
+ else if(document.getElementById('answer710').value<25){
+ alert('普通');
+ }
+ else(){
+ alert('肥満');
+ }
+}
