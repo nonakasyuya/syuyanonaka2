d9d93dae49463c5bcd40dfaf873067c75cea9cb4
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2022-12-20 23:55:57 +0900

    Create ex7-11.js

---
+++ ex7-11.js
@@ -0,0 +1,11 @@
+function add711(){
+var x =document.getElementById('nen711').value;
+//var keisan711=
+alert(Number(x)%4);
+ if(keisan711==0){
+ alert();
+ }
+ else{
+ alert('uruudosijanai');
+ }
+}
