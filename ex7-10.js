660b68bd415b33be549cda07d3ad239a5d4046e1
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2022-12-20 23:52:06 +0900

    Update ex7-10.js

--- ex7-10.js
+++ ex7-10.js
@@ -1,11 +1,19 @@
-function bmi710(){
- if(document.getElementById('answer710').value<=18.5){
+function add710(){
+var x =document.getElementById('input5').value;
+var y =document.getElementById('input6').value;
+var keisan710=Number(y)/Number(x*x)*10000;
+
+ if(keisan710 <=18.5){
+ alert(Number(y)/Number(x*x)*10000);
  alert('低体重');
  }
- else if(document.getElementById('answer710').value<25){
+ else if(keisan710 <25){
+ alert(Number(y)/Number(x*x)*10000);
  alert('普通');
  }
- else(){
+ else{
+ alert(Number(y)/Number(x*x)*10000);
  alert('肥満');
  }
 }
+
