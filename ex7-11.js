6fe73b0262a31116799dd4a3eb9325dc0e3a9409
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2022-12-21 22:08:28 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -1,11 +1,16 @@
 function add711(){
 var x =document.getElementById('nen711').value;
-alert(Number(x) % 4);
+var keisan711=Number(x)%Number('4');
+var keisan712=Number(x)%Number('100');
+var keisan713=Number(x)%Number('400');
 
-//if(keisan711==0){
-// alert('うるう年');
-// }
-// else{
-// alert('平年');
-// }
-//}
+ if(keisan712==0&&keisan713!=0){
+ alert('平年');
+ }
+ else if(keisan711==0){
+ alert('うるう年');
+}
+ else{
+ alert('平年');
+ }
+}
